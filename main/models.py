87fc818c1090d4021d4444e98e6a370f2fd5c4ba
Jab <Jab@Jabs-MacBook-Air.local>
2017-08-22 13:39:42 +0700

    done admin interface

--- main/models.py
+++ main/models.py
@@ -13,6 +13,7 @@ class Product(models.Model):
     desc = models.TextField(max_length=1000)
     price = models.FloatField()
     amount = models.IntegerField()
+    pic = models.ImageField(upload_to="media/", default="static/imgs/product_thumbnail.jpg")
 
 class Order(models.Model):
     user = models.ForeignKey(Profile)
