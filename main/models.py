7f719cb2f84835f1ce2be92e40477f61c84264dd
Jab <Jab@Jabs-MacBook-Air.local>
2017-08-22 13:07:42 +0700

    create models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
 # Create your models here.
+
+class Product(models.Model):
+    name = models.TextField(max_length=1000,null=False)
+    desc = models.TextField(max_length=1000)
+    price = models.FloatField()
+    amount = models.IntegerField()
+
+class Order(models.Model):
+    user = models.ForeignKey(Profile)
+    product = models.ForeignKey(Product)
+    amount = models.IntegerField()
+    date = models.DateTimeField()
+    status = models.IntegerField()
