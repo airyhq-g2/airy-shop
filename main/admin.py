7e15470875efd6fbc2efd78070d4001246af163c
realroy <roiboon.c@ku.th>
2017-10-04 14:55:00 +0700

    Alter models

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Profile , Product , Order
+
+from .models import UserInfo, Product, Order
+
 # Register your models here.
-admin.site.register(Profile)
+admin.site.register(UserInfo)
 admin.site.register(Product)
 admin.site.register(Order)
