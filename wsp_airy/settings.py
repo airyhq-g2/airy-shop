bcbd912b5d424b1dcb0192cc11712b88dc7e14ff
realroy <roiboon.c@ku.th>
2017-10-16 14:58:28 +0700

    Fix something again.

--- wsp_airy/settings.py
+++ wsp_airy/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = '3q=9@@*#jo+tl)*#4^mb-(f8&^q-@226(mh_t^7k_o81$tl(pu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
