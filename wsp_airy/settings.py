d3ebab885f32751ebb0a3bf5f35ca6ef2931750d
realroy <roiboon.c@ku.th>
2017-10-16 14:15:25 +0700

    Fix setting.py

--- wsp_airy/settings.py
+++ wsp_airy/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://airyhq-shop.herokuapp.com/']
 
@@ -122,6 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'main/static')
+
+STATICFILES_ROOT = (
+	os.path.join(BASE_DIR, 'main/static'),
+)
 
 LOGIN_REDIRECT_URL = '/catalogue'
 
