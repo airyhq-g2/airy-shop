a09c980e6100bc32c7627589e0a0202a98854d85
realroy <roiboon.c@ku.th>
2017-10-16 13:44:31 +0700

    Fix setting.py

--- wsp_airy/settings.py
+++ wsp_airy/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3q=9@@*#jo+tl)*#4^mb-(f8&^q-@226(mh_t^7k_o81$tl(pu'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://airyhq-shop.herokuapp.com/']
 
 
 # Application definition
