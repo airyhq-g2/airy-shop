877d26c5cf905ac71868dddb1f49afa21681529c
realroy <roiboon.c@ku.th>
2017-10-16 14:40:12 +0700

    Fix something again.

--- wsp_airy/settings.py
+++ wsp_airy/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'main/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'main/static')
 
 STATICFILES_ROOT = (
 	os.path.join(BASE_DIR, 'main/static'),
