0864806b3865f6cc3298b2161f6d8888d325e6a3
realroy <roiboon.c@ku.th>
2017-11-22 15:30:33 +0700

    Add Transaction Model.

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import UserInfo, Product, Order
+from .models import UserInfo, Product, Order, Transaction
 
 # Register your models here.
 admin.site.register(UserInfo)
 admin.site.register(Product)
+admin.site.register(Transaction)
 admin.site.register(Order)
