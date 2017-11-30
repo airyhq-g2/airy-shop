7c563a686323fed27ec718b646ea0c928ea01366
Kan <kan86197@gmail.com>
2017-11-30 18:59:31 +0700

    trytry

--- main/views.py
+++ main/views.py
@@ -43,7 +43,7 @@ class CatalogueView(ListView):
         query = self.request.GET.get('q')
         if query:
             try:
-                result = [Product.objects.get(brand__icontains=query)]
+                result = [Product.objects.filter(product__brand__icontains=query)]
             except ObjectDoesNotExist:
                 return []
         return result
