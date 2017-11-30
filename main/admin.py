4a1fa7993c11ae73c2920862da30c2ded85ba189
Tanapon Meesat <tanapon.me@ku.th>
2017-11-30 22:14:30 +0700

    change title

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.admin import AdminSite
+from django.utils.translation import ugettext_lazy
 
 from .models import UserInfo, Product, Order, Transaction , Manager
 
@@ -12,3 +14,15 @@ admin.site.register(Order)
 class ManagerAdmin(admin.ModelAdmin):
     change_list_template = 'admin/manager.html'
     date_hierachy = 'created'
+
+class MyAdminSite(AdminSite):
+    # Text to put at the end of each page's <title>.
+    site_title = ugettext_lazy('Airy Shop Admin')
+
+    # Text to put in each page's <h1> (and above login form).
+    site_header = ugettext_lazy('Business Administration')
+
+    # Text to put at the top of the admin index page.
+    index_title = ugettext_lazy('Airy Shop Admin')
+
+admin_site = MyAdminSite()
