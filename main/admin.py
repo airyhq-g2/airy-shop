d97a642d8bd86ed3a56d8d3500696d7e71964cdf
Tanapon Meesat <tanapon.me@ku.th>
2017-11-30 22:19:08 +0700

    changed title

--- main/admin.py
+++ main/admin.py
@@ -9,20 +9,12 @@ admin.site.register(UserInfo)
 admin.site.register(Product)
 admin.site.register(Transaction)
 admin.site.register(Order)
+admin.site.site_header = 'Airy Business Admin'
+admin.site.site_title = 'Airy Administration'
+admin.site.index_title = 'Airy Administration'
 
 @admin.register(Manager)
 class ManagerAdmin(admin.ModelAdmin):
     change_list_template = 'admin/manager.html'
     date_hierachy = 'created'
 
-class MyAdminSite(AdminSite):
-    # Text to put at the end of each page's <title>.
-    site_title = ugettext_lazy('Airy Shop Admin')
-
-    # Text to put in each page's <h1> (and above login form).
-    site_header = ugettext_lazy('Business Administration')
-
-    # Text to put at the top of the admin index page.
-    index_title = ugettext_lazy('Airy Shop Admin')
-
-admin_site = MyAdminSite()
